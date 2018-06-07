44727e79c480fec4f97b4ebb5745e41f8c4f0c2e
Fabio Leardini <fabioleardini@Fabios-MacBook-Pro.local>
2018-06-06 22:41:36 -0300

    Aulas 3 e 4

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -52,4 +52,75 @@ class NegociacaoService {
                 })
         });    
     }
+
+    obterNegociacoes() {
+        
+        return Promise.all([
+            this.obterNegociacoesDaSemana(),
+            this.obterNegociacoesDaSemanaAnterior(),
+            this.obterNegociacoesDaSemanaRetrasada()
+        ]).then(periodos => {
+
+            let negociacoes = periodos
+                .reduce((dados, periodo) => dados.concat(periodo), [])
+                .map(dado => new Negociacao(new Date(dado.data), dado.quantidade, dado.valor ));
+
+            return negociacoes;
+        }).catch(erro => {
+            throw new Error(erro);
+        });
+    }
+    
+    cadastra(negocicao) {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.adiciona(negociacao))
+            .then(() => 'Negociação adicionada com sucesso')
+            .catch(error => {
+                console.log(error);
+                throw new Error('Não foi possível adicionar a negociação');
+            });
+    }
+
+    lista() {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .catch(error => {
+                console.log(error);
+                throw new Error('Não foi possível obter as negociações');
+            });
+    }
+
+    apaga() {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(() => 'Negociações apagadas com sucesso')
+            .catch(error => {
+                console.log(error);
+                throw new Error('Não foi possível apagar as negociações');
+            });
+    }
+
+    importa(listaAtual) {
+
+        return this.obterNegociacoes()
+            .then(negociacoes => 
+                negociacoes.filter(negociacao => 
+                    !listaAtual.some(negociacaoExistente => 
+                        JSON.stringify(negociacao == JSON.stringify(negociacaoExistente)))
+                )
+            )
+            .catch(error => {
+                console.log(error);
+                throw new Error('Não foi possível buscar negociações para importar');
+            });
+    }
 }
